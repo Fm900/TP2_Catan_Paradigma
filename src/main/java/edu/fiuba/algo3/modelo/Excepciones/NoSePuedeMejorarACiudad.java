2cca7b273fd8ade544cca5083e8f037537e6f70a
mGoicoecheaFiuba <mgoicoechea@fi.uba.ar>
2025-11-16 17:32:10 -0300

    Agrego la excepcion para manejar errores al construir ciudades.

---
+++ src/main/java/edu/fiuba/algo3/modelo/Excepciones/NoSePuedeMejorarACiudad.java
@@ -0,0 +1,8 @@
+package edu.fiuba.algo3.modelo.Excepciones;
+
+public class NoSePuedeMejorarACiudad extends RuntimeException {
+    public NoSePuedeMejorarACiudad(String mensaje) {
+        super(mensaje);
+    }
+}
+
