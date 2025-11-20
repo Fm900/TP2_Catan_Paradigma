b4197991653edcf0bed5c5dce5d3f61f86ae54af
mGoicoecheaFiuba <mgoicoechea@fi.uba.ar>
2025-11-20 15:58:07 -0300

    Testeo el cambio de los PV al pasar de poblado a ciudad.

--- src/main/test/edu/fiuba/algo3/entrega_2/ConsumoDeRecursosYCambioDePVAlMejorarACiudad.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/ConsumoDeRecursosYCambioDePVAlMejorarACiudad.java
@@ -2,9 +2,12 @@ package edu.fiuba.algo3.entrega_2;
 
 import edu.fiuba.algo3.modelo.Exception.NoSePuedeMejorarACiudad;
 import edu.fiuba.algo3.modelo.Exception.NoTieneRecursos;
+import edu.fiuba.algo3.modelo.Juego;
 import edu.fiuba.algo3.modelo.Jugador.Jugador;
 import edu.fiuba.algo3.modelo.Jugador.Mano;
 import edu.fiuba.algo3.modelo.Recurso.*;
+import edu.fiuba.algo3.modelo.Tablero.GeneradorDeTerrenos;
+import edu.fiuba.algo3.modelo.Tablero.Tablero;
 import edu.fiuba.algo3.modelo.Tablero.Vertice.Vertice;
 import org.junit.jupiter.api.Test;
 
@@ -50,5 +53,32 @@ public class ConsumoDeRecursosYCambioDePVAlMejorarACiudad {
         assertThrows(RuntimeException.class, () -> vertice.mejorarPobladoACiudad(jugador));
     }
 
+    @Test
+    void mejorarPobladoACiudadActualizaPuntosDeVictoria() {
+        GestorDeRecursos gestor = new GestorDeRecursos(new ArrayList<>());
+        Mano mano = new Mano();
+        Jugador jugador = new Jugador(gestor, mano);
+
+        // recursos para 1 poblado
+        jugador.agregarRecurso(new Madera(), 1);
+        jugador.agregarRecurso(new Ladrillo(), 1);
+        jugador.agregarRecurso(new Grano(), 1);
+        jugador.agregarRecurso(new Lana(), 1);
+
+        // recursos para 1 ciudad
+        jugador.agregarRecurso(new Grano(), 2);
+        jugador.agregarRecurso(new Mineral(), 3);
+
+        // crear tablero y juego
+        Tablero tablero = new Tablero(new GeneradorDeTerrenos());
+        Juego juego = new Juego(List.of(jugador), new ArrayList<>(), new ArrayList<>(), tablero);
+
+        Vertice vertice = new Vertice();
+
+        vertice.construirPoblado(jugador);
+        assertEquals(1,juego.calcularPuntosTotalesDe(jugador)); // con un poblado el jugador debe tener 1 punto
+
+        vertice.mejorarPobladoACiudad(jugador);
+        assertEquals(2,juego.calcularPuntosTotalesDe(jugador)); // con una ciudad el jugador debe tener 2 puntos
+    }
 }
-    //me falta probar los PV
