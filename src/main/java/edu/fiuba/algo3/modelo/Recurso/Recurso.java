52cdf505e68c60d25f4ded8211d41488d4b66006
mGoicoecheaFiuba <mgoicoechea@fi.uba.ar>
2025-11-20 14:53:08 -0300

    Se soluciona temporalmente el error al validad cantidad de recursos.

--- src/main/java/edu/fiuba/algo3/modelo/Recurso/Recurso.java
+++ src/main/java/edu/fiuba/algo3/modelo/Recurso/Recurso.java
@@ -5,4 +5,9 @@ public abstract class Recurso {
     public abstract void agregar(int cantidad, GestorDeRecursos mazo);
 
     public abstract void eliminar(GestorDeRecursos mazo);
+
+    @Override
+    public boolean equals(Object o) {
+        return o != null && this.getClass() == o.getClass();
+    }
 }
