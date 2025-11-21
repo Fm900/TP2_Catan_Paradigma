1dc907fe4e4dbf2dfaba5af6b83f9022f4bdb677
Emiliano <emibogetti@gmail.com>
2025-11-21 20:17:35 -0300

    Agrego una Excepcion y modificaciones en Carta y Mano

--- src/main/java/edu/fiuba/algo3/modelo/Jugador/Mano.java
+++ src/main/java/edu/fiuba/algo3/modelo/Jugador/Mano.java
@@ -2,11 +2,16 @@ package edu.fiuba.algo3.modelo.Jugador;
 
 import edu.fiuba.algo3.modelo.Jugador.Cartas.Carta;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Mano {
 
     private List<Carta> cartas;
+
+    public Mano () {
+        this.cartas = new ArrayList<Carta>();
+    }
 
     public void agregar(Carta carta) {
         this.cartas.add(carta);
---
+++ src/main/test/edu/fiuba/algo3/entrega_2/CartaNoPuedeSerJugadaRecienAdquiridaTest.java
@@ -0,0 +1,32 @@
+package edu.fiuba.algo3.entrega_2;
+
+import edu.fiuba.algo3.modelo.Exception.CartaDeshabilitada;
+import edu.fiuba.algo3.modelo.Jugador.Cartas.Carta;
+import edu.fiuba.algo3.modelo.Jugador.Cartas.Deshabilitado;
+import edu.fiuba.algo3.modelo.Jugador.Cartas.Monopolio;
+import edu.fiuba.algo3.modelo.Jugador.GestorDeRecursos;
+import edu.fiuba.algo3.modelo.Jugador.Jugador;
+import edu.fiuba.algo3.modelo.Jugador.Mano;
+import edu.fiuba.algo3.modelo.Recurso.Recurso;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CartaNoPuedeSerJugadaRecienAdquiridaTest {
+
+
+    @Test
+    void test01CartaRecienLevantadaNoPuedeJugarse() {
+
+        GestorDeRecursos gestor = new GestorDeRecursos(new ArrayList<Recurso>());
+        Mano mano = new Mano();
+        Jugador jugadorQueLevanta = new Jugador(gestor,mano);
+        Carta cartaRecienLevantada = new Monopolio((new Deshabilitado()));
+        jugadorQueLevanta.agregarCarta(cartaRecienLevantada);
+
+
+        assertThrows(CartaDeshabilitada.class, () -> cartaRecienLevantada.intentarActivarEfecto(jugadorQueLevanta), "Este turno no es posible usar esta carta");
+    }
+    }
