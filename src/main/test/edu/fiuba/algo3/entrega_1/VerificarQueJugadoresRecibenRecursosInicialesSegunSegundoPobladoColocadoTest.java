81892534f8579584a59ec644bc25d1c132304f51
mGoicoecheaFiuba <mgoicoechea@fi.uba.ar>
2025-12-02 20:48:51 -0300

    Se mejoran tests.

--- src/main/test/edu/fiuba/algo3/entrega_1/VerificarQueJugadoresRecibenRecursosInicialesSegunSegundoPobladoColocadoTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/VerificarQueJugadoresRecibenRecursosInicialesSegunSegundoPobladoColocadoTest.java
@@ -1,12 +1,15 @@
 package edu.fiuba.algo3.entrega_1;
 
-import edu.fiuba.algo3.modelo.Fase.PrimerTurno;
-import edu.fiuba.algo3.modelo.Fase.SegundoTurno;
+import edu.fiuba.algo3.modelo.Fase.*;
+import edu.fiuba.algo3.modelo.Intercambio.Banca;
+import edu.fiuba.algo3.modelo.Juego;
 import edu.fiuba.algo3.modelo.Jugador.MazoDeRecursos;
 import edu.fiuba.algo3.modelo.Jugador.Jugador;
 import edu.fiuba.algo3.modelo.Jugador.Mano;
 import edu.fiuba.algo3.modelo.Recurso.*;
 import edu.fiuba.algo3.modelo.Constructores.GeneradorDeTerrenos;
+import edu.fiuba.algo3.modelo.Tablero.Arista.Arista;
+import edu.fiuba.algo3.modelo.Tablero.Arista.Vacia;
 import edu.fiuba.algo3.modelo.Tablero.Tablero;
 import edu.fiuba.algo3.modelo.Tablero.Terreno.Normal;
 import edu.fiuba.algo3.modelo.Tablero.Terreno.Terreno;
@@ -30,11 +33,22 @@ public class VerificarQueJugadoresRecibenRecursosInicialesSegunSegundoPobladoCol
     } //esto es para armar los terrenos a mano, asi se lo que debo esperar al darle los recursos al jugador por el poblado que coloca
 
     private Jugador nuevoJugadorConRecursosIniciales() {
-        MazoDeRecursos gestor = new MazoDeRecursos(new ArrayList<>(List.of(new Madera(), new Ladrillo(), new Lana(), new Grano(),new Madera(), new Ladrillo(), new Lana(), new Grano())));
+        //le damos recursos para dos poblados y dos carreteras
+        MazoDeRecursos gestor = new MazoDeRecursos(new ArrayList<>(List.of(new Madera(),new Madera(), new Madera(), new Madera(),
+                                                                           new Ladrillo(), new Ladrillo(), new Ladrillo(), new Ladrillo(),
+                                                                           new Lana(), new Lana(),
+                                                                           new Grano(), new Grano())));
         Mano mano = new Mano();
-        return new Jugador(gestor, mano,"Alex");
+        return new Jugador(gestor, mano,"Roberto");
     }
 
+    private void inicializarJuego(Tablero tablero, Jugador jugador) {
+        List<FasePrincipal> fasesPrincipales = List.of(new Dados(), new Construccion(), new Comercio(), new JugarCartas());
+        List<FaseInicial> fasesIniciales = List.of(new PrimerTurno(), new SegundoTurno()
+        );
+        Banca banca = new Banca(new ArrayList<>());
+        Juego.crearInstancia(List.of(jugador), fasesPrincipales, fasesIniciales, tablero, banca);
+    }
 
     @Test
     void Test01JugadorRecibeRecursosInicialesSegunTerrenosAdyacentes(){
@@ -43,24 +57,27 @@ public class VerificarQueJugadoresRecibenRecursosInicialesSegunSegundoPobladoCol
         Vertice verticePrimerTurno = new Vertice();
         Vertice verticeSegundoTurno = new Vertice();
 
+
         //creo un tablero de forma controlada
         Terreno terrenoConMineral = new Terreno(new Mineral(), 5, new Normal());
         terrenoConMineral.asignarVerticesAdyacentes(List.of(verticeSegundoTurno));
         Tablero tablero = new Tablero(new GeneradorDeTerrenosControlado(List.of(terrenoConMineral)));
+
+        inicializarJuego(tablero, jugador);
         List<Recurso> precio = List.of(new Mineral());
+        Arista aristaPrimerTurno = new Arista(verticePrimerTurno, new Vertice(), new Vacia());
+        Arista aristaSegundoTurno = new Arista(verticeSegundoTurno, new Vertice(), new Vacia());
 
 
         // el primer poblado no debe dar recursos, asi que no deberia tener Mineral
         PrimerTurno primerTurno = new PrimerTurno();
-        primerTurno.setVerticeParaConstruir(verticePrimerTurno);
-        primerTurno.iniciarFase(List.of(jugador),tablero);
+        primerTurno.iniciarFase(jugador, verticePrimerTurno,aristaPrimerTurno);
         assertThrows(RuntimeException.class, () -> jugador.consumirRecursos(precio), "Antes del segundo poblado no deberia poder pagar un mineral");
 
 
         // en el segundo turno construyo un poblado que me debe dar recursos
         SegundoTurno segundoTurno = new SegundoTurno();
-        segundoTurno.setVerticeParaConstruir(verticeSegundoTurno);
-        segundoTurno.iniciarFase(List.of(jugador), tablero);
+        segundoTurno.iniciarFase(jugador, verticeSegundoTurno, aristaSegundoTurno);
         assertDoesNotThrow(() -> jugador.consumirRecursos(precio), "Despues del segundo poblado deberia poder pagar un mineral");
 
     }
--- src/main/test/edu/fiuba/algo3/entrega_2/VerificarElConsumoDeRecursosYLaCorrectaColocaciónDeUnaCarretera.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/VerificarElConsumoDeRecursosYLaCorrectaColocaciónDeUnaCarretera.java
@@ -1,41 +1,39 @@
 package edu.fiuba.algo3.entrega_2;
-
 import edu.fiuba.algo3.modelo.Jugador.MazoDeRecursos;
 import edu.fiuba.algo3.modelo.Jugador.Jugador;
 import edu.fiuba.algo3.modelo.Jugador.Mano;
-import edu.fiuba.algo3.modelo.Recurso.Ladrillo;
-import edu.fiuba.algo3.modelo.Recurso.Madera;
-import edu.fiuba.algo3.modelo.Recurso.Recurso;
+import edu.fiuba.algo3.modelo.Recurso.*;
 import edu.fiuba.algo3.modelo.Tablero.Arista.Arista;
 import edu.fiuba.algo3.modelo.Tablero.Arista.Vacia;
 import edu.fiuba.algo3.modelo.Tablero.Vertice.Vertice;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 public class VerificarElConsumoDeRecursosYLaCorrectaColocaciónDeUnaCarretera {
-    Vertice vertice1;
-    Vertice vertice2;
-    Arista arista;
-    Jugador jugador;
-    List<Recurso> precio;
-    MazoDeRecursos gestor;
-    Mano mano;
+
 
     @Test
     public void test01VerificarConsumoDeRecursosAlConstruirUnaCarretera(){
-        precio = new ArrayList<>(List.of(new Madera(), new Ladrillo()));
-        gestor = new MazoDeRecursos(precio);
-        jugador = new Jugador(gestor, mano,"Alex");
-        vertice1 = new Vertice();
-        vertice2 = new Vertice();
-        arista = new Arista(vertice1, vertice2, new Vacia());
+        List<Recurso> recursosIniciales = new ArrayList<>(List.of(new Madera(), new Ladrillo(), new Lana(), new Grano(), new Madera(), new Ladrillo()));
+        List<Recurso> precioCarretera = List.of(new Madera(), new Ladrillo());
 
-        assertDoesNotThrow(() -> jugador.consumirRecursos(precio), "No tienes suficiente");
-        assertThrows(RuntimeException.class, () -> jugador.consumirRecursos(precio), "No tienes suficiente");
+        MazoDeRecursos gestor = new MazoDeRecursos(recursosIniciales);
+        Mano mano = new Mano();
+        Jugador jugador = new Jugador(gestor, mano, "El chaqueño palavecino");
+        Vertice vertice1 = new Vertice();
+        Vertice vertice2 = new Vertice();
+        Arista arista = new Arista(vertice1, vertice2, new Vacia());
+
+        assertDoesNotThrow(() -> vertice1.construirPobladoInicial(jugador), "Deberia poder construir un poblado inicial con los recursos disponibles");
+        assertDoesNotThrow(() -> arista.construirCarretera(jugador), "Deberia poder construirse una carretera conectada a un vertice del jugador");
+        assertThrows(RuntimeException.class, () -> jugador.consumirRecursos(precioCarretera), "Tras construir una carretera no deberia poder pagar otra");
+        assertThrows(RuntimeException.class, () -> arista.construirCarretera(jugador), "No se puede construir otra carretera sobre una arista ya ocupada");
+
+
+
     }
 }
