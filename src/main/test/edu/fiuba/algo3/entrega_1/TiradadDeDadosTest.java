b1e2f9e8f3e32dad0bbb53bd7c542525027215e9
mGoicoecheaFiuba <mgoicoechea@fi.uba.ar>
2025-12-04 16:57:30 -0300

    Correccion de tests.

--- src/main/test/edu/fiuba/algo3/entrega_1/TiradadDeDadosTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/TiradadDeDadosTest.java
@@ -1,6 +1,11 @@
 package edu.fiuba.algo3.entrega_1;
 
 import edu.fiuba.algo3.controllers.ManejoTurnos;
+import edu.fiuba.algo3.modelo.Juego;
+import edu.fiuba.algo3.modelo.Tablero.Arista.Arista;
+import edu.fiuba.algo3.modelo.Tablero.Tablero;
+import edu.fiuba.algo3.modelo.Tablero.Terreno.Terreno;
+import edu.fiuba.algo3.modelo.Tablero.Vertice.Vertice;
 import edu.fiuba.algo3.modelo.Turnos.Fase.Dados;
 import edu.fiuba.algo3.modelo.Intercambio.Banca;
 import edu.fiuba.algo3.modelo.Jugador.Cartas.Carta;
@@ -8,6 +13,7 @@ import edu.fiuba.algo3.modelo.Jugador.Jugador;
 import edu.fiuba.algo3.modelo.Jugador.Mano;
 import edu.fiuba.algo3.modelo.Jugador.MazoDeRecursos;
 import edu.fiuba.algo3.modelo.Recurso.Recurso;
+import edu.fiuba.algo3.modelo.Turnos.Normal;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -22,9 +28,14 @@ public class TiradadDeDadosTest {
         Jugador jugador1 = new Jugador(gestor1, mano1,"Alex");
         Dados faseDeDados = new Dados();
         List<Carta> cartas = new ArrayList<>();
+        Banca.reset();
         Banca banca = Banca.crearBanca(new ArrayList<Recurso>(), cartas);
-        /*Act*/
-        faseDeDados.ejecutar(jugador1, new ManejoTurnos(new ArrayList<>(List.of(jugador1))));
+        Tablero tableroVacio = new Tablero(new ArrayList<Terreno>(), new ArrayList<Vertice>(), new ArrayList<Arista>());
+        Juego.reset();
+        Juego.crearInstancia(List.of(jugador1), tableroVacio, banca);
+        ManejoTurnos manejoTurnos = new ManejoTurnos(new ArrayList<>(List.of(jugador1)));
+        manejoTurnos.cambiarTurno(new Normal());
+        faseDeDados.ejecutar(jugador1, manejoTurnos);
         int tirada = faseDeDados.getTirada();
         /*Assert*/
         assert(tirada > 2 && tirada <= 12);
--- src/main/test/edu/fiuba/algo3/entrega_1/VerificarQueJugadoresRecibenRecursosInicialesSegunSegundoPobladoColocadoTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/VerificarQueJugadoresRecibenRecursosInicialesSegunSegundoPobladoColocadoTest.java
@@ -30,63 +30,56 @@ public class VerificarQueJugadoresRecibenRecursosInicialesSegunSegundoPobladoCol
 
     class GeneradorDeTerrenosControlado extends GeneradorDeTerrenos {
         private final List<Terreno> terrenos;
-        public GeneradorDeTerrenosControlado(List<Terreno> terrenos) {this.terrenos = terrenos;}
+        public GeneradorDeTerrenosControlado(List<Terreno> terrenos) { this.terrenos = terrenos; }
         @Override
-        public List<Terreno> generar() {return terrenos;}
-    } //esto es para armar los terrenos a mano, asi se lo que debo esperar al darle los recursos al jugador por el poblado que coloca
-
-    private Jugador nuevoJugadorConRecursosIniciales() {
-        //le damos recursos para dos poblados y dos carreteras
-        MazoDeRecursos gestor = new MazoDeRecursos(new ArrayList<>(List.of(new Madera(),new Madera(), new Madera(), new Madera(),
-                                                                           new Ladrillo(), new Ladrillo(), new Ladrillo(), new Ladrillo(),
-                                                                           new Lana(), new Lana(),
-                                                                           new Grano(), new Grano())));
-        Mano mano = new Mano();
-        return new Jugador(gestor, mano,"Roberto");
+        public List<Terreno> generar() { return terrenos; }
     }
 
-    private void inicializarJuego(Tablero tablero, Jugador jugador) {
-        List<Fase> fasesPrincipales = List.of(new Dados(), new Construccion(), new Comercio(), new JugarCartas());
-        List<Turno> fasesIniciales = List.of(new Primer(), new Segundo()
-        );
-        List<Carta> cartas = new ArrayList<>();
-        Banca banca = Banca.crearBanca(new ArrayList<>(), cartas);
-        Juego.crearInstancia(List.of(jugador), new Tablero(new ArrayList<Terreno>(), new ArrayList<Vertice>(), new ArrayList<Arista>()), Banca.crearBanca(List.of(new Madera()), cartas));
-
+    private Jugador nuevoJugadorConRecursosIniciales() {
+        // recursos para dos poblados y dos carreteras
+        MazoDeRecursos gestor = new MazoDeRecursos(new ArrayList<>(List.of(new Madera(), new Madera(), new Madera(), new Madera(), new Ladrillo(), new Ladrillo(), new Ladrillo(), new Ladrillo(), new Lana(), new Lana(), new Grano(), new Grano())));
+        Mano mano = new Mano();
+        return new Jugador(gestor, mano,"Roberto");
     }
 
     @Test
     void Test01JugadorRecibeRecursosInicialesSegunTerrenosAdyacentes(){
         Juego.reset();
         Jugador jugador = nuevoJugadorConRecursosIniciales();
+        Vertice verticePrimerTurno = new Vertice(1, 1.0, 1.0);
+        Vertice verticeSegundoTurno = new Vertice(2, 2.0, 2.0);
 
-        Vertice verticePrimerTurno = new Vertice(1,1,1);
-        Vertice verticeSegundoTurno = new Vertice(2,2,2);
 
-
-        //creo un tablero de forma controlada
         Terreno terrenoConMineral = new Terreno(new Mineral(), 5, new Normal());
         terrenoConMineral.asignarVerticesAdyacentes(List.of(verticeSegundoTurno));
+
+
         Tablero tablero = new Tablero(new GeneradorDeTerrenosControlado(List.of(terrenoConMineral)));
 
-        inicializarJuego(tablero, jugador);
+        List<Carta> cartas = new ArrayList<>();
+        Banca banca = Banca.crearBanca(new ArrayList<>(), cartas);
+        Juego.crearInstancia(List.of(jugador), tablero, banca);
+
         List<Recurso> precio = List.of(new Mineral());
-        Arista aristaPrimerTurno = new Arista(verticePrimerTurno, new Vertice(0,1,1), new Vacia());
-        Arista aristaSegundoTurno = new Arista(verticeSegundoTurno, new Vertice(0,1,1), new Vacia());
+
+        Arista aristaPrimerTurno = new Arista(verticePrimerTurno, new Vertice(0, 1.0, 1.0), new Vacia());
+        Arista aristaSegundoTurno = new Arista(verticeSegundoTurno, new Vertice(0, 1.0, 1.0), new Vacia());
 
 
-        // el primer poblado no debe dar recursos, asi que no deberia tener Mineral
-        Primer primerTurno = new Primer();
         List<Jugador> jugadores = new ArrayList<>();
         jugadores.add(jugador);
-        primerTurno.construir(new ManejoTurnos(jugadores), verticePrimerTurno,aristaPrimerTurno);
+        ManejoTurnos manejoTurnos = new ManejoTurnos(jugadores);
+
+        // 1) Primer turno: construye poblado inicial + carretera, pero no recibe recursos iniciales
+        Primer primerTurno = new Primer();
+        primerTurno.construir(manejoTurnos, verticePrimerTurno, aristaPrimerTurno);
+
         assertThrows(RuntimeException.class, () -> jugador.consumirRecursos(precio), "Antes del segundo poblado no deberia poder pagar un mineral");
 
-
-        // en el segundo turno construyo un poblado que me debe dar recursos
+        // 2) Segundo turno: construye poblado inicial + carretera en vertice adyacente al terreno de mineral
         Segundo segundoTurno = new Segundo();
-        segundoTurno.construir(new ManejoTurnos(jugadores), verticeSegundoTurno, aristaSegundoTurno);
-        assertDoesNotThrow(() -> jugador.consumirRecursos(precio), "Despues del segundo poblado deberia poder pagar un mineral");
+        segundoTurno.construir(manejoTurnos, verticeSegundoTurno, aristaSegundoTurno);
 
+        assertDoesNotThrow(() -> jugador.consumirRecursos(precio), "Despues del segundo poblado deberia poder pagar un mineral");
     }
 }
